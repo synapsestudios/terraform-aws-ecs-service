894d7581f3b6d660fad44cb0e77d29cb14b40c35
Chris Shiplet <chris@nearengine.com>
2023-02-01 18:41:21 -0700

    Update db.tf

--- db.tf
+++ db.tf
@@ -1,6 +1,6 @@
 module "database" {
   count                           = var.use_database_cluster ? 1 : 0
-  source                          = "git::https://github.com/synapsestudios/terraform-aws-rds-aurora-cluster.git?ref=a8f4ea6a6a5886b2bfeb43d1403a7abfe8e3f0e2"
+  source                          = "git::https://github.com/synapsestudios/terraform-aws-rds-aurora-cluster.git?ref=v0.0.7"
   availability_zones              = var.azs
   database_subnets                = var.subnets
   db_cluster_parameter_group_name = "default.aurora-postgresql14"
