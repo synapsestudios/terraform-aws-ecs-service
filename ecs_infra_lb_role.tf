3454ead2f033162e0d1096b3b0957915fead91eb
Devin Lauderdale <devin.lauderdale@synapsestudios.com>
2025-09-25 22:48:24 -0500

    fix: add role name instead of role object and fix policy attachment resource name

--- ecs_infra_lb_role.tf
+++ ecs_infra_lb_role.tf
@@ -16,7 +16,7 @@ data "aws_iam_policy_document" "ecs_infrastructure_trust_policy" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "cognito" {
-  role       = aws_iam_role.ecs_infra_lb_role
+resource "aws_iam_role_policy_attachment" "ecs_infra_policy" {
+  role       = aws_iam_role.ecs_infra_lb_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonECSInfrastructureRolePolicyForLoadBalancers"
 }
