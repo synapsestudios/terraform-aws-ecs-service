5f2805b2e15b38d87de10050c0489cd036472d09
Devin Lauderdale <devin.lauderdale@synapsestudios.com>
2025-09-25 21:52:48 -0500

    fix: reduce role name for infra lb role so that prefix will still work

--- ecs_infra_lb_role.tf
+++ ecs_infra_lb_role.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ecs_infra_lb_role" {
-  name_prefix        = "${substr(var.service_name, 0, 24)}-ecs-infrastructure-role-for-lbs"
+  name_prefix        = "${substr(var.service_name, 0, 24)}-ecs-infra-lbs"
   assume_role_policy = data.aws_iam_policy_document.ecs_infrastructure_trust_policy.json
 }
 
