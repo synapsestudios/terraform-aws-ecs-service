76b5daff20ac7294ea95ef262b9878e41fa2a49b
Devin Lauderdale <devin.lauderdale@synapsestudios.com>
2025-10-01 13:11:59 -0500

    fix: remove incorrect test prefix from var

--- alb_target_group.tf
+++ alb_target_group.tf
@@ -112,7 +112,7 @@ resource "aws_lb_listener_rule" "green" {
 
   condition {
     host_header {
-      values = [format("test-%", test-var.hostname)]
+      values = [format("test-%", var.hostname)]
     }
   }
 }
